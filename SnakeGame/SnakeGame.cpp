d599a5b5847b3a49734bb9c0f19a13f07f5954cf
Giang Nguyễn Tấn <20521261@ms.uit.edu.vn>
2021-12-07 14:44:26 +0700

    Add initGame()

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -19,9 +19,30 @@ struct Point {
 	int x,y,x0,y0;
 };
 
-void loadingScreen();
-void run();
+struct HighScore {
+	int score;
+	char name[30];
+};
+
+void initGame ();
+bool checkPoint ();
+void drawPoint (int x,int y,int r);
+void moveSnake ();
+void drawSnake ();
+void drawFood ();
+void drawGame ();
+void classic();
+void modern();
+void mainLoop (void (*gloop)());
+void run ();
+void changeDirecton (int x);
+void showHighScore();
+void getHighScore();
+void checkHighScore(int score);
+void initScore();
+bool isEmpty();
 void showText(int x,int y,char *str);
+void showTextBackground(int x,int y,char *str,int color);
 
 void showText(int x,int y,char *str){
 	int c = getcolor();
@@ -68,14 +89,14 @@ void run (){
     int bk = getbkcolor();
     setfillstyle (SOLID_FILL,0);
 	int a[] = {15,350,30,350,30,345,50,355,30,365,30,360,15,360,15,350};
-    fillpoly (12,a);
+    fillpoly (8,a);
     settextstyle (4,0,3); outtextxy (70,340,"Right");
     int b[] = {15,410,30,400,30,405,45,405,45,415,30,415,30,420,15,410};
-    fillpoly (12,b);outtextxy (70,400,"Left");
+    fillpoly (8,b);outtextxy (70,400,"Left");
 	int c[] = {235,370,235,355,230,355,240,340,250,355,245,355,245,370,235,370};
-	fillpoly (12,c);outtextxy (265,340,"Up");
+	fillpoly (8,c);outtextxy (265,340,"Up");
 	int d[] = {235,400,235,415,230,415,240,430,250,415,245,415,245,400,235,400};
-	fillpoly (12,d);outtextxy (265,400,"Down");
+	fillpoly (8,d);outtextxy (265,400,"Down");
 	bar (50,450,170,480);setcolor (15);settextstyle(1,0,1);setbkcolor(0);outtextxy(60,455,"SPACE");setbkcolor(bk);
 	setcolor(0);settextstyle (4,0,3);outtextxy(200,450,"Pause/Resum");
 	bar (50,500,170,530);setcolor (15);settextstyle(1,0,1);setbkcolor(0);outtextxy(80,505,"ESC");setbkcolor(bk);
@@ -86,7 +107,7 @@ void run (){
 	bar (25,25,415,215);
 	
 	setcolor (2);
-	Sleep(10000);
+	Sleep(2000);
 		
 }
 	
