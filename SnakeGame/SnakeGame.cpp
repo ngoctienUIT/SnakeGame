b8c6188dbc262f945c0b8b1c04e2d9650f9c1c4d
Giang Nguyễn Tấn <20521261@ms.uit.edu.vn>
2021-12-07 12:31:07 +0700

    Add loading screen

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -18,42 +18,36 @@ using namespace std;
 struct Point {
 	int x,y,x0,y0;
 };
-struct HighScore {
-	int score;
-	char name[30];
-};
 
-void initGame ();
-bool checkPoint ();
-void drawPoint (int x,int y,int r);
-void moveSnake ();
-void drawSnake ();
-void drawFood ();
-void drawGame ();
-void classic();
-void modern();
-void mainLoop (void (*gloop)());
-void run ();
-void changeDirecton (int x);
-void showHighScore();
-void getHighScore();
-void checkHighScore(int score);
-void initScore();
-bool isEmpty();
-void showText(int x,int y,char *str);
-void showTextBackground(int x,int y,char *str,int color);
+void loadingScreen();
+void run();
 
+void loadingScreen() {
+	int x = 0;
+	char *s = new char[5];
+	while (x <= 100){
+		cleardevice();
+		setcolor(10);settextstyle(4,0,5);
+		outtextxy (60,200,"Game Loading...");
+		sprintf(s,"%d",x);
+		setcolor(4);
+		outtextxy (550,200, strcat(s,"%"));
+		if (x == 100)
+			delay (500);
+		
+		delay (rand()% 20);
+		x++;
+	}
+}
 
-int level;
-bool endGame;
-int snakeLength;
-Point snake[100];
-Point direction;
-Point food;
-const int DIRECTION	= 10;
-HighScore  highscore[5];
-char* score_str = new char[20];
+void run (){
+	initwindow (800,600);
+	loadingScreen();
+}
+
 
 int main (){
 	cout<< "Welcome to 3PGT's Snake Game!";
+	run();
+	return 0;
 }
