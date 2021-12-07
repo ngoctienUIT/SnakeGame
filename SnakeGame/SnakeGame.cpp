e49af08b8b2a119db50f34f435147ceeacec027d
Phúc Nguyễn Hoàng <20521768@gm.uit.edu.vn>
2021-12-07 19:49:38 +0700

    Add changeDirection func

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -213,7 +213,38 @@ void run (){
 	Sleep(10000);
 }
 	
-
+void changeDirecton (int x)
+{
+	switch(x){
+                case 72: 
+                    if (direction.y != DIRECTION) {
+                    	PlaySound(TEXT("beep.wav"), NULL, SND_ASYNC);
+            			direction.y = -DIRECTION; direction.x = 0;
+        			}
+                    break;
+                case 80: 
+                	if (direction.y != -DIRECTION) {
+                		PlaySound(TEXT("beep.wav"), NULL, SND_ASYNC);
+            			direction.y = DIRECTION; direction.x = 0;
+        			}
+                   break;
+                case 77:
+                    if (direction.x != -DIRECTION) {
+                    	PlaySound(TEXT("beep.wav"), NULL, SND_ASYNC);
+            			direction.x = DIRECTION; direction.y = 0;
+        			}
+                    break;
+                case 75:
+                    if (direction.x != DIRECTION) {
+                    	PlaySound(TEXT("beep.wav"), NULL, SND_ASYNC);
+            			direction.x = -DIRECTION; direction.y = 0;
+        			}
+                    break;
+                case 27:
+                	endGame = true;
+					break;
+            }
+}
 
 int main () {
 	cout<< "Welcome to 3PGT's Snake Game!";
