987051ebfd455ba0ba6f9ef85141d239df944d34
Phúc Nguyễn Hoàng <20521768@gm.uit.edu.vn>
2021-12-09 09:23:39 +0700

    Add classic function

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -27,7 +27,6 @@ struct HighScore {
 void initGame ();
 bool checkPoint ();
 void drawPoint (int x,int y,int r);
-void moveSnake ();
 void drawSnake ();
 void drawFood ();
 void drawGame ();
@@ -244,6 +243,37 @@ void changeDirecton (int x)
                 	endGame = true;
 					break;
             }
+}
+
+void classic(){
+    for (int i = 0; i < snakeLength; i++) {
+        if (i == 0) {
+            snake[0].x0 = snake[0].x;snake[0].y0 = snake[0].y;
+			snake[0].x += direction.x;
+			snake[0].y += direction.y;
+        }else{
+            snake[i].x0 = snake[i].x;snake[i].y0 = snake[i].y;
+			snake[i].x = snake[i-1].x0;snake[i].y = snake[i-1].y0;
+        }
+         
+        if (snake[i].x >= MAXX) snake[i].x = MINX + 10;
+        if (snake[i].x <= MINX) snake[i].x = MAXX - 10;
+        if (snake[i].y >= MAXY) snake[i].y = MINY + 10;
+        if (snake[i].y <= MINY) snake[i].y = MAXY - 10;
+         
+        if (i != 0 && (snake[0].x == snake[i].x && snake[0].y == snake[i].y)) endGame = true;
+    }
+    if (snake[0].x == food.x && snake[0].y == food.y){
+		snake[snakeLength].x = snake[snakeLength-1].x0;snake[snakeLength].y = snake[snakeLength-1].y0;
+		snakeLength++;
+		PlaySound(TEXT("eatFood.wav"), NULL, SND_ASYNC);
+		//Random again food if sanke ate food
+		srand ( time(NULL));
+        do{
+        	food.x = (rand() % (39) + 3)*10;
+    		food.y = (rand() % (19) + 3)*10;
+		}while (checkPoint() == false);
+	}
 }
 
 int main () {
