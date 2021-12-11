4857111776f19344bb9e5e547a8f014e20a0ac5b
Giang Nguyễn Tấn <20521261@ms.uit.edu.vn>
2021-12-11 14:52:29 +0700

    Add showHighScore
    Description

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -358,6 +358,32 @@ void mainLoop (void (*xxx)())
     }
 }
 
+//ham hien thi diem cao tu file
+void showHighScore(){
+	FILE *f;
+	f = fopen("highscore.txt", "r");
+	char ch[20];
+	settextstyle (1,0,5);
+	setcolor (15);
+	outtextxy (150,50,"HIGH SCORE");
+	settextstyle (1,0,4);
+	int y = 150,count = 0;
+	while (!feof(f)){
+		if (count == 10) break;
+		count++;
+		fscanf(f, "%s", ch);
+   		if (count%2 == 1){
+   			setcolor (10);
+   			outtextxy (180,y,ch);
+   			y+=50;
+		}else{
+			setcolor (12);
+			outtextxy (500,y-50,ch);	   	
+		}
+	}
+	fclose(f);
+}
+
 int main () {
 	cout<< "Welcome to 3PGT's Snake Game!";
 	run();
