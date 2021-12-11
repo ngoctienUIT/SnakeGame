d72059c5038139edb65e73d2fe2c5f96500503c8
Ngọc Tiến Trần <94039133+ngoctienUIT@users.noreply.github.com>
2021-12-11 15:57:25 +0700

    add run and isEmpty function
    Thêm 2 hàm run và isEmpty

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -79,7 +79,8 @@ void loadingScreen()
 {
 	int x = 0;
 	char *s = new char[5];
-	while (x <= 100){
+	while (x <= 100)
+	{
 		cleardevice();
 		setcolor(10);settextstyle(4,0,5);
 		outtextxy (60,200,"Game Loading...");
@@ -97,7 +98,8 @@ void loadingScreen()
 
 bool checkPoint ()
 {
-	for (int i = 0;i < snakeLength;i++){
+	for (int i = 0;i < snakeLength;i++)
+	{
 		if (food.x == snake[i].x && food.y == snake[i].y)
 		return false;
 	}
@@ -116,10 +118,11 @@ void drawSnake ()
 {
 	setfillstyle (1,10);
 	int dem = 0;
-	for (int i = 0;dem < snakeLength;i++){
-			drawPoint(snake[i].x, snake[i].y,5);
-			dem++;
-		}
+	for (int i = 0;dem < snakeLength;i++)
+	{
+		drawPoint(snake[i].x, snake[i].y,5);
+		dem++;
+	}
 	//Xoa dot cuoi cung cua than ran moi khi no di chuyen
 	int x = getcolor();
 	setfillstyle (1,BACKGROUND);
@@ -150,11 +153,12 @@ void drawGame ()
 	int x = getcolor ();
 	setcolor (2);
 	sprintf(score_str,"%d",snakeLength*level*10-level*30);    //Hien thi diem
-   settextstyle(4,0,3);
-   outtextxy(450,100,"SCORE:");
-   setcolor(12);
-   outtextxy(600,20,score_str);
-   setcolor (x);
+   	settextstyle(4,0,3);
+   	outtextxy(450,100,"SCORE:");
+   	settextstyle(4,0,3);
+   	setcolor(12);
+   	outtextxy(600,100,score_str);
+   	setcolor (x);
 }
 
 void initGame()
@@ -204,52 +208,219 @@ void initGame()
 	srand ( time(NULL));
 	
 	//Khoi tao trai cay
-   	/*do{
+   	do{
         food.x = (rand() % (39) + 3)*10;
     	food.y = (rand() % (19) + 3)*10;
-	}while (checkPoint() == false);*/
+	}while (checkPoint() == false);
 
 }
 
-void run ()
-{
+void run (){
 	initwindow (800,600);
-	//loadingScreen();
-	initGame();
-	settextstyle(1,0,6);
-	setcolor (10);
-	showText(110, 110, "Hien thi text");
+	initScore();
+	int x = 0;
+	char *s = new char[5];
+	while (x <= 100){
+		cleardevice();
+		setcolor (10);settextstyle(4,0,5);outtextxy (60,200,"Game Loading...");
+		sprintf (s,"%d",x);
+		setcolor (4);
+		outtextxy (550,200,strcat(s,"%"));
+		if (x == 100)
+			delay (500);
+		delay (20);
+		x++;
+	}
+	
+	while (true){
+	x1:
+	int bk = getbkcolor();
+	cleardevice();
+	setbkcolor(0);
+	cleardevice();
 	clearmouseclick(WM_LBUTTONDOWN);
-	
-	showTextBackground(110, 210, "Hien thi text", 12);
-	
-	Sleep(10000);
+	PlaySound(TEXT("Audio\\faded.wav"), NULL , SND_ASYNC);
+	while (!ismouseclick(WM_LBUTTONDOWN) || mousex() < 200 || mousey() > 465 || mousey() < 190 || mousex() > 580 ){
+		settextstyle(1,0,6);
+		setcolor (10);
+		outtextxy(120,70,"GAME MENU");
+		settextstyle(10,0,5);
+		showText (220,200,"NEW GAME");
+		showText (220,270,"HIGH SCORE");
+		showText (220,340,"QUIT GAME");
+		showText (220,410,"ABOUT");
+	}
+	clearmouseclick(WM_LBUTTONDOWN);
+	if (mousey() <= 255){
+		showTextBackground(220,200,"NEW GAME",15);
+		x2:
+		setbkcolor(0);
+		cleardevice();
+		int nameFunction;
+		clearmouseclick(WM_LBUTTONDOWN);
+		while (!ismouseclick(WM_LBUTTONDOWN) || mousex() < 290 || mousex() > 520 || mousey() < 240 || mousey() > 400){
+			settextstyle(1,0,6);setcolor(10);
+			outtextxy (150,150,"GAME TYPE");
+			setcolor(15);settextstyle(1,0,4);
+			showTextBackground(250,250,"CLASSIC",0);
+			showTextBackground (250,350,"MODERN",0);
+		}
+		if (ismouseclick(WM_LBUTTONDOWN) && mousex() >= 290 && mousex() <= 520 && mousey() >= 240 && mousey() <= 400){
+			if(mousey() <= 300) nameFunction = 1;
+			else nameFunction = 2;
+		}
+		setbkcolor(0);
+		cleardevice();
+		setcolor(4);settextstyle(4,0,3);
+		outtextxy(10,310,"YOUR CHOICE :");
+		settextstyle (4,0,6);
+		setcolor (10);
+		outtextxy (150,200,"LEVEL 1 - 5");
+		setcolor(4);settextstyle(4,0,3);
+		outtextxy(10,310,"YOUR CHOICE :");
+		int xx = 330;
+		char str1[2];
+		clearmouseclick(WM_LBUTTONDOWN);
+		while (!ismouseclick(WM_LBUTTONDOWN) || mousex() < 300 || mousex() > 790 || mousey() < 280 || mousey() > 350) 
+		for(int i = 1;i <= 5;i++){
+			sprintf(str1,"%d",i);
+			settextstyle(4,0,4);
+			showTextBackground(xx,300,str1,0);
+			xx+= 100;
+		}
+		if (ismouseclick(WM_LBUTTONDOWN) && mousex() >= 300 && mousex() <= 790 && mousey() >= 280 && mousey() <= 350){
+			if(mousex() <= 390) level = 1;
+			else if(mousex() <= 490) level = 2;
+			else if(mousex() <= 590) level = 3;
+			else if(mousex() <= 690) level = 4;
+			else level = 5;
+		}
+		PlaySound(TEXT("Audio\\start.wav"), NULL , SND_ASYNC);
+		initGame();
+		while (!endGame){
+			//Change delay folow level
+		delay (250 - (level-1)*40);
+		(nameFunction == 1) ? mainLoop(classic):mainLoop(modern);
+		drawGame();
+	}
+		PlaySound(TEXT("Audio\\endGame.wav"), NULL , SND_ASYNC);
+		settextstyle (1,0,4);
+		setcolor (0);
+		outtextxy (100,110,"GAME OVER!");
+		setcolor (8);
+		settextstyle (1,0,2);
+		outtextxy (100,160,"Your Score : ");
+		setcolor(10);
+		outtextxy (300,160,score_str);
+		//Here is check high score
+		checkHighScore(snakeLength*level*10-level*30);
+		setcolor(10);settextstyle(4,0,5);
+		outtextxy (100,200,"Play Again ?");
+		settextstyle(4,0,3);
+		setcolor(1);
+		clearmouseclick(WM_LBUTTONDOWN);
+		while (!ismouseclick(WM_LBUTTONDOWN))
+		{
+			showTextBackground(150,270,"YES",12);showTextBackground(300,270,"NO",7);
+		}
+		if(ismouseclick(WM_LBUTTONDOWN))
+		{
+			if (mousex() >= 140 && mousex() <= 230 && mousey() >= 260 && mousey() <= 310)
+			{
+				showTextBackground(150,270,"YES",7);
+				delay(100);
+				goto x2;
+			}
+			else if(mousex() >= 290 && mousex() <= 360 && mousey() >= 260 && mousey() <= 310)
+			{
+				showTextBackground(300,270,"NO",12);
+				delay(100);
+				goto x1;
+			}
+        	}
+    	}
+		else if (mousey() <= 325)
+		{
+			showTextBackground(220,270,"HIGH SCORE",15);
+			cleardevice();
+			showHighScore();
+			while (!ismouseclick(WM_LBUTTONDOWN))
+			{
+				delay(0);
+			}
+			goto x1;
+		}
+		else if(mousey() <= 395)
+		{
+			showTextBackground (220,340,"QUIT GAME",15);
+			exit(0);
+		}
+		else
+		{
+			
+		cleardevice();
+		settextstyle (1,0,4);
+		setcolor (15);
+		int x = getcolor();
+		while(!ismouseclick(WM_LBUTTONDOWN))
+		{
+			settextstyle(10,0,6);setcolor(15);
+			outtextxy(290,20,"ABOUT");
+			settextstyle(10,0,4);setcolor(9);
+			outtextxy(60,100,"TRUONG DAI HOC CONG NGHE THONG TIN");
+			settextstyle(10,0,5);
+			showText(290,150,"DE TAI");
+			settextstyle(10,0,2);
+			showText(250,200,"TRO CHOI \"RAN SAN MOI\"");
+			settextstyle(10,0,3);
+			showText(220,270,"SINH VIEN THUC HIEN:");
+			settextstyle(10,0,2);
+			showText(200,320,"TRAN NGOC TIEN - 20520808");
+			settextstyle(10,0,3);
+			showText(200,370,"NGUYEN TAN GIANG - 20521261");
+			settextstyle(10,0,3);
+			showText(200,420,"LE HOANG PHUC - 20521763");
+			settextstyle(10,0,3);
+			showText(200,470,"VU THIEN PHU - 20521758");
+			settextstyle(10,0,3);
+			showText(200,520,"NGUYEN HOANG PHUC - 20521768");
+		}
+		setcolor(x);
+		settextstyle (1,0,2);
+		setcolor (4);
+		goto x1;
+		}
+	}
 }
 	
 void changeDirecton (int x)
 {
 	switch(x){
                 case 72: // di len neu hien tai dang sang trai hoac phai
-                    if (direction.y != DIRECTION) {
-                    	PlaySound(TEXT("beep.wav"), NULL, SND_ASYNC);
+                    if (direction.y != DIRECTION) 
+					{
+                    	PlaySound(TEXT("Audio\\beep.wav"), NULL, SND_ASYNC);
             			direction.y = -DIRECTION; direction.x = 0;
         			}
                     break;
                 case 80: // di xuong neu hien tai dang sang trai hoac phai
-                	if (direction.y != -DIRECTION) {
-                		PlaySound(TEXT("beep.wav"), NULL, SND_ASYNC);
+                	if (direction.y != -DIRECTION) 
+					{
+                		PlaySound(TEXT("Audio\\beep.wav"), NULL, SND_ASYNC);
             			direction.y = DIRECTION; direction.x = 0;
         			}
                    break;
                 case 77: //sang phai neu hien tai dang len hoac xuong
-                    if (direction.x != -DIRECTION) {
-                    	PlaySound(TEXT("beep.wav"), NULL, SND_ASYNC);
+                    if (direction.x != -DIRECTION) 
+					{
+                    	PlaySound(TEXT("Audio\\beep.wav"), NULL, SND_ASYNC);
             			direction.x = DIRECTION; direction.y = 0;
         			}
                     break;
                 case 75: //sang trai neu hien tai dang len hoac xuong
-                    if (direction.x != DIRECTION) {
-                    	PlaySound(TEXT("beep.wav"), NULL, SND_ASYNC);
+                    if (direction.x != DIRECTION) 
+					{
+                    	PlaySound(TEXT("Audio\\beep.wav"), NULL, SND_ASYNC);
             			direction.x = -DIRECTION; direction.y = 0;
         			}
                     break;
@@ -262,12 +433,16 @@ void changeDirecton (int x)
 void classic()
 {
 	// che do choi di xuyen qua tuong
-    for (int i = 0; i < snakeLength; i++) {
-        if (i == 0) {
+    for (int i = 0; i < snakeLength; i++) 
+	{
+        if (i == 0) 
+		{
             snake[0].x0 = snake[0].x;snake[0].y0 = snake[0].y;
 			snake[0].x += direction.x;
 			snake[0].y += direction.y;
-        }else{
+        }
+		else
+		{
             snake[i].x0 = snake[i].x;snake[i].y0 = snake[i].y;
 			snake[i].x = snake[i-1].x0;snake[i].y = snake[i-1].y0;
         }
@@ -280,14 +455,16 @@ void classic()
         // ket thuc neu ran cham vao than
         if (i != 0 && (snake[0].x == snake[i].x && snake[0].y == snake[i].y)) endGame = true;
     }
-    if (snake[0].x == food.x && snake[0].y == food.y){
+    if (snake[0].x == food.x && snake[0].y == food.y)
+	{
 		snake[snakeLength].x = snake[snakeLength-1].x0;snake[snakeLength].y = snake[snakeLength-1].y0;
 		snakeLength++;
-		PlaySound(TEXT("eatFood.wav"), NULL, SND_ASYNC);
+		PlaySound(TEXT("Audio\\eatFood.wav"), NULL, SND_ASYNC);
 		
 		//Tao thuc an moi
 		srand ( time(NULL));
-        do{
+        do
+		{
         	food.x = (rand() % (39) + 3)*10;
     		food.y = (rand() % (19) + 3)*10;
 		}while (checkPoint() == false);
@@ -297,13 +474,16 @@ void classic()
 void modern()
 {
 	// che do choi khong di xuyen tuong
-	for (int i = 0;i < snakeLength;i++){
-		if (i == 0){
+	for (int i = 0;i < snakeLength;i++)
+	{
+		if (i == 0)
+		{
 			snake[0].x0 = snake[0].x;snake[0].y0 = snake[0].y;
 			snake[0].x += direction.x;
 			snake[0].y += direction.y;
 		}
-		else {
+		else 
+		{
 			snake[i].x0 = snake[i].x;snake[i].y0 = snake[i].y;
 			snake[i].x = snake[i-1].x0;snake[i].y = snake[i-1].y0;
 		}
@@ -315,14 +495,16 @@ void modern()
 		// ket thuc neu ran cham vao than
 		if (i != 0 && snake[0].x == snake[i].x && snake[0].y == snake[i].y)	 endGame = true;
 	}
-	if (snake[0].x == food.x && snake[0].y == food.y){
+	if (snake[0].x == food.x && snake[0].y == food.y)
+	{
 		snake[snakeLength].x = snake[snakeLength-1].x0;snake[snakeLength].y = snake[snakeLength-1].y0;
 		snakeLength++;
-		PlaySound(TEXT("eatFood.wav"), NULL, SND_ASYNC);
+		PlaySound(TEXT("Audio\\eatFood.wav"), NULL, SND_ASYNC);
 		
 		//Tao thuc an moi
 		srand ( time(NULL));
-		do{
+		do
+		{
         	food.x = (rand() % (39) + 3)*10;
     		food.y = (rand() % (19) + 3)*10;
 		}while (checkPoint() == false);
@@ -333,17 +515,21 @@ void modern()
 void mainLoop (void (*xxx)())
 {
 	int x = getcolor();
-	if (!kbhit()){
+	if (!kbhit())
+	{
 		xxx();
 	}
-	else{
+	else
+	{
 		char ch;
         ch = getch();
 		if (ch == -32) ch = getch();
-		if ( ch == 32){
+		if ( ch == 32)
+		{
 			setcolor(0);outtextxy (100,100,"Game Pause");setcolor(x);
 			ch = getch();
-			while (ch != 32){
+			while (ch != 32)
+			{
 				ch = getch();
 				delay(0);
 			}
@@ -358,7 +544,8 @@ void mainLoop (void (*xxx)())
 }
 
 //ham hien thi diem cao tu file
-void showHighScore(){
+void showHighScore()
+{
 	FILE *f;
 	f = fopen("highscore.txt", "r");
 	char ch[20];
@@ -367,15 +554,19 @@ void showHighScore(){
 	outtextxy (150,50,"HIGH SCORE");
 	settextstyle (1,0,4);
 	int y = 150,count = 0;
-	while (!feof(f)){
+	while (!feof(f))
+	{
 		if (count == 10) break;
 		count++;
 		fscanf(f, "%s", ch);
-   		if (count%2 == 1){
+   		if (count%2 == 1)
+		{
    			setcolor (10);
    			outtextxy (180,y,ch);
    			y+=50;
-		}else{
+		}
+		else
+		{
 			setcolor (12);
 			outtextxy (500,y-50,ch);	   	
 		}
@@ -384,10 +575,12 @@ void showHighScore(){
 }
 
 //ham luu diem cao vao file 
-void getHighScore (){
+void getHighScore ()
+{
 	FILE *f;
 	f = fopen("highscore.txt", "w");
-	for (int i = 0;i < 5;i++){
+	for (int i = 0;i < 5;i++)
+	{
 		fputs(highscore[i].name,f);
 		fputs(" ",f);
 		fprintf(f,"%d",highscore[i].score);
@@ -399,51 +592,65 @@ void getHighScore (){
 void initScore(){
 	// neu khong co file highcore.txt,khoi tao mang highscore voi name = PLAYER va score = 0
 		//if (isEmpty()){
-		if(1){
-			for (int i = 0;i < 5;i++){
-			strcpy(highscore[i].name,"PLAYER");
-			highscore[i].score = 0;
+		if(1)
+		{
+			for (int i = 0;i < 5;i++)
+			{
+				strcpy(highscore[i].name,"PLAYER");
+				highscore[i].score = 0;
 			}
 			//getHighScore();
 		}
 		// neu co file thi doc file va luu vao mang highscore
-		else{
+		else
+		{
 			char arr[20];
 			int count = 0;
 			FILE *f;
 			f = fopen("highscore.txt", "r");
-			for (int i = 0;i < 5;){
-				while (!feof(f)){
+			for (int i = 0;i < 5;)
+			{
+				while (!feof(f))
+				{
 					count++;
 					fscanf(f, "%s", arr);
-				   if (count%2 == 1){
+				   if (count%2 == 1)
+				   {
 				   		strcpy(highscore[i].name,arr);
 						//printf ("%s\t",ch);
 					}
-					else{
+					else
+					{
 						highscore[i].score = atoi(arr);
 						i++;
-						}
-				   }
+					}
 				}
 			}
+		}
 }
-void checkHighScore (int _score){
+void checkHighScore (int _score)
+{
 	// kiem tra diem nguoi choi co duoc xep vao bang diem cao k, neu co thi luu vao file
 	char _name[20]={""};
-	for (int i = 0;i < 5;i++){
-		if (_score > highscore[i].score){
+	for (int i = 0;i < 5;i++)
+	{
+		if (_score > highscore[i].score)
+		{
 			//to do sth
 			settextstyle(1,0,3);
-				for (int j = 0;j < 50;j++){
-					if (j%2 == 0){
+				for (int j = 0;j < 50;j++)
+				{
+					if (j%2 == 0)
+					{
 						setcolor (14);
 						if (i == 0)
 						outtextxy(460,100,"BEST SCORE");
 						else
 						outtextxy(460,100,"HIGH SCORE");
 						delay(100);
-					}else{
+					}
+					else
+					{
 						setcolor (9);
 						if (i == 0)
 						outtextxy(460,100,"BEST SCORE");
@@ -459,16 +666,20 @@ void checkHighScore (int _score){
 				int x = 0;
 				char str[2];
 				str[1] = 0;
-				while (ch1 != 13 && x < 10){
-					do{
+				while (ch1 != 13 && x < 10)
+				{
+					do
+					{
 						ch1 = getch();
-					}while (ch1 < 65 && ch1 >90 || ch1 < 97 && ch1 > 132);
+					}
+					while (ch1 < 65 && ch1 >90 || ch1 < 97 && ch1 > 132);
 					x++;
 					str[0] = ch1;
 					strcat(_name,str);
 					outtextxy(540,150,_name);
 				}
-			for (int j = 4;j > i;j--){
+			for (int j = 4;j > i;j--)
+			{
 				strcpy(highscore[j].name,highscore[j-1].name);
 				highscore[j].score = highscore[j-1].score;
 			}
@@ -480,7 +691,23 @@ void checkHighScore (int _score){
 	//goi ham luu vao file
 	//getHighScore();
 }
-int main () {
+
+//kiem tra xem file highscore.txt co rong hay khong
+bool isEmpty()
+{
+	FILE *fp;
+	long size;
+	fp = fopen("highscore.txt","r");
+	if(fp){
+		fseek (fp, 0, SEEK_END);
+        size = ftell(fp);
+		fclose(fp);
+	}
+	return(size == 0);
+}
+
+int main () 
+{
 	cout<< "Welcome to 3PGT's Snake Game!";
 	run();
 	return 0;
