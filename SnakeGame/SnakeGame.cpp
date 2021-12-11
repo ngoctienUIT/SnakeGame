2cec14c1a5742926a06b173ac395af48884616e1
Giang Nguyễn Tấn <20521261@ms.uit.edu.vn>
2021-12-11 14:55:32 +0700

    Add getHighScore()

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -384,6 +384,19 @@ void showHighScore(){
 	fclose(f);
 }
 
+//ham luu diem cao vao file 
+void getHighScore (){
+	FILE *f;
+	f = fopen("highscore.txt", "w");
+	for (int i = 0;i < 5;i++){
+		fputs(highscore[i].name,f);
+		fputs(" ",f);
+		fprintf(f,"%d",highscore[i].score);
+		fputs("\n",f);
+	}
+	fclose(f);
+}
+
 int main () {
 	cout<< "Welcome to 3PGT's Snake Game!";
 	run();
