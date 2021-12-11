dc5384f2e78c1a1cb886c79039b52bcb31f17906
Phúc Nguyễn Hoàng <20521768@gm.uit.edu.vn>
2021-12-11 14:59:44 +0700

    Add initScore and checkHighScore function

--- SnakeGame/SnakeGame.cpp
+++ SnakeGame/SnakeGame.cpp
@@ -215,7 +215,6 @@ void run ()
 {
 	initwindow (800,600);
 	//loadingScreen();
-;
 	initGame();
 	settextstyle(1,0,6);
 	setcolor (10);
@@ -358,6 +357,90 @@ void mainLoop (void (*xxx)())
     }
 }
 
+void initScore(){
+	// neu khong co file highcore.txt,khoi tao mang highscore voi name = PLAYER va score = 0
+		//if (isEmpty()){
+		if(1){
+			for (int i = 0;i < 5;i++){
+			strcpy(highscore[i].name,"PLAYER");
+			highscore[i].score = 0;
+			}
+			//getHighScore();
+		}
+		// neu co file thi doc file va luu vao mang highscore
+		else{
+			char arr[20];
+			int count = 0;
+			FILE *f;
+			f = fopen("highscore.txt", "r");
+			for (int i = 0;i < 5;){
+				while (!feof(f)){
+					count++;
+					fscanf(f, "%s", arr);
+				   if (count%2 == 1){
+				   		strcpy(highscore[i].name,arr);
+						//printf ("%s\t",ch);
+					}
+					else{
+						highscore[i].score = atoi(arr);
+						i++;
+						}
+				   }
+				}
+			}
+}
+void checkHighScore (int _score){
+	// kiem tra diem nguoi choi co duoc xep vao bang diem cao k, neu co thi luu vao file
+	char _name[20]={""};
+	for (int i = 0;i < 5;i++){
+		if (_score > highscore[i].score){
+			//to do sth
+			settextstyle(1,0,3);
+				for (int j = 0;j < 50;j++){
+					if (j%2 == 0){
+						setcolor (14);
+						if (i == 0)
+						outtextxy(460,100,"BEST SCORE");
+						else
+						outtextxy(460,100,"HIGH SCORE");
+						delay(100);
+					}else{
+						setcolor (9);
+						if (i == 0)
+						outtextxy(460,100,"BEST SCORE");
+						else
+						outtextxy(460,100,"HIGH SCORE");
+						delay(100);
+					}
+				}
+				settextstyle(1,0,2);
+				setcolor (4);outtextxy(430,150,"Player:");
+				delay(100);
+				char ch1;
+				int x = 0;
+				char str[2];
+				str[1] = 0;
+				while (ch1 != 13 && x < 10){
+					do{
+						ch1 = getch();
+					}while (ch1 < 65 && ch1 >90 || ch1 < 97 && ch1 > 132);
+					x++;
+					str[0] = ch1;
+					strcat(_name,str);
+					outtextxy(540,150,_name);
+				}
+			for (int j = 4;j > i;j--){
+				strcpy(highscore[j].name,highscore[j-1].name);
+				highscore[j].score = highscore[j-1].score;
+			}
+			strcpy(highscore[i].name,_name);
+			highscore[i].score = _score;
+			break;
+		}
+	}
+	//goi ham luu vao file
+	//getHighScore();
+}
 int main () {
 	cout<< "Welcome to 3PGT's Snake Game!";
 	run();
